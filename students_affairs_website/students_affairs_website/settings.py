acc9a95268a0f019356da8eb2aaa5bd0a552e3c2
Abobakr7 <102465288+Abobakr7@users.noreply.github.com>
2023-05-24 20:09:43 +0300

    Update settings.py 404 will add it later

--- students_affairs_website/students_affairs_website/settings.py
+++ students_affairs_website/students_affairs_website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g6-8^y$b+qx&g-h81&w$#i-#n436q-qe!q2bdw@ilg6%d5wq4f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
