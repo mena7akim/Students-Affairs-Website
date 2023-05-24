1b20c4e9ab35cb04e70d307ba1924b9f4c1e3640
Mina Hakim <102185230+mena7akim@users.noreply.github.com>
2023-05-25 01:56:03 +0300

    Added update view, URL and updated html file

--- students_affairs_website/pages/urls.py
+++ students_affairs_website/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.general_home, name="general_home"),
     path('home/', views.home, name='home'),
+    path('update/', views.update, name='update'),
 ]
--- students_affairs_website/pages/views.py
+++ students_affairs_website/pages/views.py
@@ -4,13 +4,17 @@ from django.template import loader
 from .models import Student
 
 def home(request):
-  students = Student.objects.all().values()
   template = loader.get_template('home.html')
-  context = {
-    'students' : students,
-  }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render())
 
 def general_home(request):
     template = loader.get_template('general-home.html')
     return HttpResponse(template.render())
+
+def update(request):
+    template = loader.get_template('update.html')
+    students = Student.objects.all().values()
+    context = {
+      'students' : students,
+    }
+    return HttpResponse(template.render(context, request))
