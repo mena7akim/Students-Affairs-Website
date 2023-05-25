f874156ef3b0a2ba0ddfe68cb74b643854eb1bd9
SohaAshraff <102482621+SohaAshraff@users.noreply.github.com>
2023-05-26 01:23:16 +0300

    Add files via upload

---
+++ students_affairs_website/pages/static/JS/dep.js
@@ -0,0 +1,10 @@
+let name = document.getElementById("name");
+let id = document.getElementById("id");
+let depart = document.getElementById("department");
+let btn = document.getElementById("submit");
+
+
+btn.addEventListener("click", (event) => {
+  
+  alert("The value of the department has been updated");
+});
