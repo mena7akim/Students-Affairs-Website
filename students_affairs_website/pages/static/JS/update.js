9fadfa96967e1fa65f208f6a7283da0f290839f6
Mina Hakim <102185230+mena7akim@users.noreply.github.com>
2023-06-01 12:41:10 +0300

    updated update.js

--- students_affairs_website/pages/static/JS/update.js
+++ students_affairs_website/pages/static/JS/update.js
@@ -6,33 +6,3 @@ document.getElementById("filter").onclick = () => {
     filterSection.style.display = "flex";
   }
 };
-
-$(document).ready(function() {
-    // Handle form submission
-    $('#form').on('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission behavior
-
-        // Get the selected filter values
-        var department = $('#department').val();
-        var level = $('#level').val();
-        var sort = $('#sort').val();
-
-        // Create an AJAX request
-        $.ajax({
-            url: '/filter-data/',  // URL to your filter view function
-            type: 'GET',
-            data: {
-                department: department,
-                level: level,
-                sort: sort
-            },
-            success: function(response) {
-                // Update the content with the filtered results
-                $('#filtered-results').html(response);
-            },
-            error: function(xhr, textStatus, error) {
-                console.log(error);
-            }
-        });
-    });
-});
